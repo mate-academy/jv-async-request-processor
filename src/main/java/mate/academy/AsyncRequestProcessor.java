2de839f415bd5a7e3c1c1b35f4427d894016395f
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-12-26 20:27:29 +0200

    added fixex

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -4,34 +4,28 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 public class AsyncRequestProcessor {
     private final Map<String, UserData> cache = new ConcurrentHashMap<>();
-    private final Function<String, UserData> createUserDetails;
+    private final Function<String, UserData> userDataFunction;
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
-        createUserDetails = userId -> new UserData(userId, "Details for " + userId);
+        userDataFunction = userId -> new UserData(userId, "Details for " + userId);
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return CompletableFuture.supplyAsync(() -> fetchUserData(userId), executor)
-                .exceptionally(throwable -> {
-                    System.out.println(throwable);
-                    return null;
-                });
-    }
-
-    private UserData fetchUserData(String userId) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(300);
-
-            return cache.computeIfAbsent(userId, createUserDetails);
-        } catch (InterruptedException ex) {
-            throw new RuntimeException("Userdata fetching failed");
+        if (!cache.containsKey(userId)) {
+            return CompletableFuture.supplyAsync(() -> userDataFunction.apply(userId), executor)
+                    .whenComplete((userData, exception) -> {
+                        if (exception != null) {
+                            throw new RuntimeException(exception);
+                        }
+                        cache.put(userId, userData);
+                    });
         }
+        return CompletableFuture.supplyAsync(() -> cache.get(userId));
     }
 }
