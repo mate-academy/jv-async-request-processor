843f993ca132c2a20a1adf1c9fcb28f43f1ca63a
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-01-01 17:11:56 +0200

    implemented AsyncRequestProcessor class

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,35 @@
 package mate.academy;
 
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Random random = new Random();
+    private Map<String, UserData> map = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (map.containsKey(userId)) {
+            return CompletableFuture.supplyAsync(() -> map.get(userId));
+        }
+        UserData userData = new UserData(userId, "This user from "
+                + Country.values()[random.nextInt(Country.values().length)]);
+        map.put(userId, userData);
+        return CompletableFuture.supplyAsync(() -> map.get(userId));
+    }
+    enum Country {
+        UKRAINE,
+        USA,
+        UK,
+        CANADA,
+        MOLDOVA,
+        QATAR
     }
 }
