ebb094d82626fde42844eaf36236fd0668793187
denysSmeiNewBom <den.smei.ds@gmail.com>
2024-01-04 23:47:01 +0200

    Fix getUserById

--- src/main/java/mate/academy/service/impl/UserDataServiceMockImpl.java
+++ src/main/java/mate/academy/service/impl/UserDataServiceMockImpl.java
@@ -1,30 +1,11 @@
 package mate.academy.service.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import mate.academy.UserData;
 import mate.academy.service.UserDataService;
 
 public class UserDataServiceMockImpl implements UserDataService {
-    private final List<UserData> db = new ArrayList(
-            Arrays.asList(new UserData("user1", "Details"),
-                    new UserData("user2", "Details"),
-                    new UserData("user3", "Details"),
-                    new UserData("user4", "Details"))
-    );
-
     @Override
     public UserData getUserById(String id) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        UserData userData = db.stream()
-                .filter(data -> data.userId() == id)
-                .findFirst()
-                .orElse(null);
-        return userData;
+        return new UserData(id, "Details");
     }
 }
