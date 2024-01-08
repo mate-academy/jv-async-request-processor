93cf130f573da427e32d3faae13ecec2c871fc41
-admin- <viktoriiamaherko@gmail.com>
2024-01-08 16:24:22 +0200

    Develop a Java application that processes user requests asynchronously

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,31 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final String USER_DETAILS = "Details for ";
     private final Executor executor;
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cache.containsKey(userId)) {
+            return CompletableFuture.supplyAsync(() -> cache.get(userId), executor);
+        }
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(200);
+                cache.put(userId, new UserData(userId, USER_DETAILS + userId));
+                return cache.get(userId);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, executor);
     }
 }
