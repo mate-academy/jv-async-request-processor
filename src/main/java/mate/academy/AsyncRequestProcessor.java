16706828f78268fc1b86c9924d24140478016f9d
Max <mpbakunov@gmail.com>
2024-01-26 22:11:49 +0100

    first commit

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,9 +1,11 @@
 package mate.academy;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private final ConcurrentHashMap<String, UserData> cache = new ConcurrentHashMap<>();
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
@@ -11,6 +13,27 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        CompletableFuture<UserData> completableFuture = new CompletableFuture<>();
+
+        completableFuture.completeAsync(() -> {
+            if (cache.containsKey(userId)) {
+                return cache.get(userId);
+            }
+
+            UserData userData = getUserDataFromBase(userId);
+            cache.put(userId, userData);
+
+            return userData;
+        }, executor);
+        return completableFuture;
+    }
+
+    private UserData getUserDataFromBase(String userId) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new UserData(userId, "Details for " + userId);
     }
 }
