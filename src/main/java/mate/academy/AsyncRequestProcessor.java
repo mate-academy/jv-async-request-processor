6fba45c80a27f29952d54d69a82894666188d01a
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-27 20:01:43 +0200

    Added implementation for AsyncRequestProcessor

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,9 +1,12 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final Map<String, UserData> cache = new ConcurrentHashMap<>();
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
@@ -11,6 +14,25 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            if (cache.get(userId) != null) {
+                return cache.get(userId);
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            UserData userData = new UserData(
+                    userId,
+                    String.format("Details for user with id: %s", userId)
+            );
+
+            cache.put(userId, userData);
+
+            return userData;
+        }, executor);
     }
 }
