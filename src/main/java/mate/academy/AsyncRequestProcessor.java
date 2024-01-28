d7df39583caafc2f8ebfe9bcdb4de849ce657022
Igor Korolevich <igorkorolj@gmail.com>
2024-01-28 20:03:35 +0200

    Implemented a method processRequest(String userId)

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,39 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static int count;
     private final Executor executor;
+    private Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cache.containsKey(userId)) {
+            return CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException("InterruptedException");
+                }
+                return cache.get(userId);
+            });
+        }
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException("InterruptedException");
+                    }
+                    return new UserData(userId, "user number is" + count++);
+                }
+        ).whenComplete((user, throwable) -> cache.put(userId, user));
     }
 }
