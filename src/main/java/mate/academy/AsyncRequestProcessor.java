7481c44f7d35a53b3f24c2bffcea9c9ed87d35ac
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2024-01-29 14:54:07 +0200

    implemented userdata request

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,39 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final String DETAILS_FOR = "Details for ";
     private final Executor executor;
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> findUserData(userId), executor);
+    }
+
+    private UserData findUserData(String userId) {
+        if (cache.containsKey(userId)) {
+            return cache.get(userId);
+        }
+        UserData userData = findUserDataFromDB(userId);
+        cache.put(userId, userData);
+        return userData;
+    }
+
+    private UserData findUserDataFromDB(String userId) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+        return new UserData(userId, DETAILS_FOR + userId);
     }
 }
