d12c3aaf99f7778a86eebeca6d04f5d59da5571e
Yura Uniyat <yurauniyat123@gmail.com>
2024-01-30 14:01:09 +0200

    Homework java-mt---Asynchronous user request processing and caching.

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,9 +1,12 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
@@ -11,6 +14,25 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        UserData cachedResult = cache.get(userId);
+        if (cachedResult != null) {
+            System.out.println("Served from cache: " + cachedResult);
+            return CompletableFuture.completedFuture(cachedResult);
+        }
+        return CompletableFuture.supplyAsync(() -> simulateProcessing(userId), executor)
+                .thenApply(result -> {
+                    cache.put(userId, result);
+                    return result;
+                });
+    }
+
+    private UserData simulateProcessing(String userId) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String details = "Details for " + userId;
+        return new UserData(userId, details);
     }
 }
