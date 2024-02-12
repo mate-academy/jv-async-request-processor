4e46b6c3d3f5df4234cb14097b3cedfe39a41d58
Roman Menzheres <roma.menzheres@gmail.com>
2024-02-12 18:16:42 +0200

    Implemented method 'processRequest' in 'AsyncRequestProcessor'

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,41 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final String USER_DETAILS = "Details for ";
+    private static final int DEFAULT_SLEEPING_TIME = 200;
     private final Executor executor;
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cache.containsKey(userId)) {
+            return CompletableFuture.supplyAsync(() -> cache.get(userId), executor);
+        }
+
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    try {
+                        Thread.sleep(DEFAULT_SLEEPING_TIME);
+                        return new UserData(userId, USER_DETAILS + userId);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        throw new RuntimeException(e);
+                    }
+                }, executor)
+                .whenComplete((userData, throwable) -> {
+                    if (throwable == null) {
+                        cache.put(userId, userData);
+                    } else {
+                        throw new RuntimeException(throwable.getMessage());
+                    }
+                });
     }
 }
