c657fad711516778e9bb5fcbb6d022c3d5152887
Yura Khilko <yurakhilko@gmail.com>
2024-02-21 16:00:07 +0200

    added solution.

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,9 +1,12 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final Map<String, UserData> cache = new ConcurrentHashMap<>();
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
@@ -11,6 +14,29 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
+        return CompletableFuture.supplyAsync(() -> cache.get(userId))
+                .thenApply(data -> {
+                    if (data == null) {
+                        throw new ValueIsAbsentException();
+                    }
+                    return data;
+                })
+                .exceptionallyAsync(throwable -> handleThrowable(throwable, userId));
+    }
+
+    private static class ValueIsAbsentException extends RuntimeException {
+    }
+
+    private UserData handleThrowable(Throwable throwable, String userId) {
+        if (throwable.getCause().getClass() == ValueIsAbsentException.class) {
+            return saveUserInfoToCache(userId);
+        }
         return null;
+    }
+
+    private UserData saveUserInfoToCache(String userId) {
+        UserData userData = new UserData(userId, "Details for " + userId);
+        cache.put(userId, userData);
+        return userData;
     }
 }
