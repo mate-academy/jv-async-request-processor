3aa04e1eb7290869fa6052c5d77a8000a12c32db
Vlad Bondarev <bondarev.21@icloud.com>
2024-04-11 17:13:22 +0300

    hw

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,27 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private Map<String, UserData> map;
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
+        map = new ConcurrentHashMap<>();
+
+    }
+
+    private static UserData apply(String id) {
+        return new UserData(id, "Details for " + id);
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(
+                () -> map.computeIfAbsent(userId, AsyncRequestProcessor::apply),
+                executor);
     }
 }
