1c1043efbb6e3a323e4b7f61ea419d95665c3858
Wasja Zejkan <vasazejkan5@gmail.com>
2024-04-19 22:51:09 +0300

    Implemented the processRequest(userId) method with caching support in the AsyncRequestProcessor class

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,32 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cache.containsKey(userId)) {
+            return CompletableFuture.completedFuture(cache.get(userId));
+        }
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            UserData userData = new UserData(userId, "Details for user: " + userId);
+            cache.put(userId, userData);
+            return userData;
+        }, executor);
     }
 }
