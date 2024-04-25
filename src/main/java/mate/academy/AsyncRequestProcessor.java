c5e8ad47f1c60afcd4e23c064530942fd046cb9d
Artem Kostenko <a_kostenco@yahoo.com>
2024-04-25 21:57:09 +0300

    async request processor first commit

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,37 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private Map<String, UserData> cache;
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
+        this.cache = new ConcurrentHashMap<>();
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(()-> fillAndReturnUserData(userId), executor);
     }
+
+    private UserData fillAndReturnUserData(String userId) {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (cache.containsKey(userId)) {
+            return cache.get(userId);
+        }
+        UserData userData = new UserData(userId, "Details for " + userId);
+        cache.put(userId, userData);
+        return userData;
+    }
+
+
 }
