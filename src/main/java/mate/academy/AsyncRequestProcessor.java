b0ce047f62c4a84e445766e2a9079bdb3e77aff9
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-05-12 12:22:15 +0300

    implemented async user request porcessor

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,31 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Map<String, UserData> cache;
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
+        cache = new ConcurrentHashMap<>();
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            UserData cachedUserData = cache.get(userId);
+            if (cachedUserData != null) {
+                return cachedUserData;
+            }
+            UserData userData = new UserData(
+                    userId,
+                    "Details for " + userId
+            );
+            cache.put(userId, userData);
+            return userData;
+        }, executor);
     }
 }
