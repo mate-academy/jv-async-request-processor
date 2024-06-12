9e8450c6e9924dd2ca260e105073e7d3f517a385
Jakub Sekowski <sekowskijakub@gmail.com>
2024-06-12 11:30:37 +0200

    First solution of jv-async-request-processor

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,33 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cache.containsKey(userId)) {
+            return CompletableFuture.completedFuture(cache.get(userId));
+        }
+        return CompletableFuture.supplyAsync(() -> getNotCachedUser(userId), executor);
+    }
+
+    private UserData getNotCachedUser(String userId) {
+        try {
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        UserData notCachedUserData = new UserData(userId, "Details for " + userId);
+        cache.put(userId, notCachedUserData);
+        return notCachedUserData;
     }
 }
