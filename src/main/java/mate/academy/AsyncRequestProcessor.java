fc02bc5d4a21d7a606d05ab421dbb8a082923390
TonyH277 <gajjduktoxa@gmail.com>
2024-06-20 17:10:49 +0300

    update AsyncRequestProcessor

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
+import java.util.function.Function;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
@@ -14,12 +15,11 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        if (cache.containsKey(userId)) {
-            return CompletableFuture.supplyAsync(() -> cache.get(userId), executor);
-        }
-        UserData userData = new UserData(userId, "Details for user" + userId);
-        cache.put(userId, userData);
         return CompletableFuture.supplyAsync(()
-                -> userData, executor);
+                -> cache.computeIfAbsent(userId, this::getDetailsForUser), executor);
+    }
+
+    private UserData getDetailsForUser(String userId) {
+        return new UserData(userId, "Details for user " + userId);
     }
 }
