5273bf17f6f471de081f7e04448f6b82ea721cbd
Tupadzaky <vladyslavkru@ext.inditex.com>
2024-06-20 09:25:41 +0300

    Implemented AsyncRequestProcessor

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,33 @@
 package mate.academy;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
+import lombok.SneakyThrows;
+
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Map<String, UserData> map = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> getUserData(userId), executor);
+    }
+
+    private UserData getUserData(String userId) {
+        return map.computeIfAbsent(userId, key -> {
+            // simulate data fetching
+            try {
+                TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(2500));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+            return new UserData(key, "Details for %s".formatted(key));
+        });
     }
 }
