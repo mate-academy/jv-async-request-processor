4deadc96aaa7e15d9ca7a1cb67ccd9a323478e0f
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-02 23:56:31 +0300

    feat: implement async request processor

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,33 @@
 package mate.academy;
 
+import static javax.management.timer.Timer.ONE_SECOND;
+
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final String DETAILS_PREFIX = "Details for ";
+
     private final Executor executor;
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            cache.computeIfAbsent(userId, id -> {
+                try {
+                    Thread.sleep(ONE_SECOND);
+                } catch (InterruptedException exception) {
+                    throw new RuntimeException(exception);
+                }
+                return new UserData(id, DETAILS_PREFIX + id);
+            });
+            return cache.get(userId);
+        }, executor);
     }
 }
