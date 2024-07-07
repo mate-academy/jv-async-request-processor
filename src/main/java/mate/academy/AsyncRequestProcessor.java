ab06ace4c059c5e103139c3f7035fac4c4ffaa44
Pavlo Betsa <bigworld2303@gmail.com>
2024-07-07 10:49:17 +0300

    Fixed after mentor review

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 public class AsyncRequestProcessor {
-    private final static int SLEEP_TIME = 1;
+    private static final int SLEEP_TIME = 1;
     private final Executor executor;
     private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
