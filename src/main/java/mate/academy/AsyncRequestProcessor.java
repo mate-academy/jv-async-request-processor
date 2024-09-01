afd27c9f6a6b097c97b5903fa348967638a9328f
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-09-01 15:21:50 +0200

    solution

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -18,23 +18,21 @@ public class AsyncRequestProcessor {
             return CompletableFuture.completedFuture(cache.get(userId));
         } else {
             return CompletableFuture.supplyAsync(() -> {
-                        try {
-                            Thread.sleep(200);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            throw new RuntimeException("Thread was interrupted", e);
-                        }
-                        UserData userData = new UserData(userId, "Details for " + userId);
-                        return userData;
-                    }, executor)
-                    .thenApply(userData -> {
-                        cache.put(userData.userId(), userData);
-                        return userData;
-                    })
-                    .exceptionally(e -> {
-                        System.out.println("Error processing the request for user: " + userId);
-                        return new UserData(userId, "Error processing request");
-                    });
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException("Thread was interrupted", e);
+                }
+                UserData userData = new UserData(userId, "Details for " + userId);
+                return userData;
+            }, executor).thenApply(userData -> {
+                cache.put(userData.userId(), userData);
+                return userData;
+            }).exceptionally(e -> {
+                System.out.println("Error processing the request for user: " + userId);
+                return new UserData(userId, "Error processing request");
+            });
         }
     }
 }
