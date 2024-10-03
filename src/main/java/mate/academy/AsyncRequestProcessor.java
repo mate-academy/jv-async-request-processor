bf9582cf5f5e6ce2aeb6e6a966e6d3a45dfacb85
IvanovYurii <ivanovyura76@gmail.com>
2024-10-03 22:06:16 +0300

    Solution of this task

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,32 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    public Map<String, UserData> cashData = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cashData.containsKey(userId)) {
+            return CompletableFuture.completedFuture(cashData.get(userId));
+        }
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return new UserData(userId, "Details for " + userId);
+        }, executor).thenApply(userData -> {
+            cashData.put(userId, userData);
+            return userData; // Return the UserData object
+        });
     }
 }
