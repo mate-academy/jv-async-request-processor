2f43aa8a4ab8cce8129ea4ad045518a235db78d9
Artem Klishch <artemklishch@gmail.com>
2024-10-27 13:58:48 +0200

    feat: made processRequest method

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,30 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+
+    private Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        try {
+            Thread.sleep(100);
+            if (cache.containsKey(userId)) {
+                return CompletableFuture.completedFuture(cache.get(userId));
+            }
+            UserData userData = new UserData(userId, "Details for " + userId);
+            cache.put(userId, userData);
+            return CompletableFuture.supplyAsync(() -> userData, executor);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
