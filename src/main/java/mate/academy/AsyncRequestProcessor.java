d28796402a27efa74e010b3adcfbb9e4245034d7
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-12-12 13:52:51 +0200

    async cashed request

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,32 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        for (String key : cache.keySet()) {
+            if (key.equals(userId)) {
+                return CompletableFuture.supplyAsync(() -> cache.get(key));
+            } else {
+                continue;
+            }
+        }
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        cache.put(userId, new UserData(userId, "Details for " + userId));
+        return CompletableFuture.supplyAsync(() -> cache.get(userId));
     }
 }
