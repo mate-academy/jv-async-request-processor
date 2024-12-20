4001fa66c86c4e9b8ee5ffff639538f3656db27b
Zorii Bohdan <bodkazorii@gmail.com>
2024-12-20 21:41:06 +0200

    solved

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,27 @@
 package mate.academy;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Random random = new Random();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        try {
+            Thread.sleep(random.nextInt(200));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return CompletableFuture.supplyAsync(() -> createUserData(userId), executor);
+    }
+
+    private UserData createUserData(String userId) {
+        return new UserData(userId, "Details for " + userId);
     }
 }
