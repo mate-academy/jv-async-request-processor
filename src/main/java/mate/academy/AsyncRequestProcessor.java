fbe547338a60793fdc7ad82b10180ed906d01b60
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2025-02-11 20:56:02 +0100

    Done Asynchronous request processing and caching. Attempt 1.0 - implemented method processRequest()

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,21 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Map<String, CompletableFuture<UserData>> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        String userDetails = "Details for " + userId;
+        return cache.computeIfAbsent(userId, id -> CompletableFuture
+                .supplyAsync(() -> new UserData(userId, userDetails)));
     }
 }
