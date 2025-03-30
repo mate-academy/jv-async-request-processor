6a24360211aa2099104b77a1b262ad2fe95a4984
Igor Neminuschii <igorgorslav@gmail.com>
2025-03-30 19:08:21 +0300

    fixed line length

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
     public static void main(String[] args) {
         // Feel free to play with AsyncRequestProcessor in this main method if you want
-        ExecutorService executor = ForkJoinPool.commonPool(); // Provide implementation that fits your needs
+        ExecutorService executor = ForkJoinPool.commonPool();
         AsyncRequestProcessor asyncRequestProcessor = new AsyncRequestProcessor(executor);
 
         // Simulating multiple concurrent requests
