1806dcda5456ce1f875e0631a4707fdefd163278
Marharyta Salii <margarita12122015@gmail.com>
2025-04-04 22:44:08 +0300

    resolved

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,32 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final int SLEEP_TIME = 200;
     private final Executor executor;
+    private Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            if (cache.containsKey(userId)) {
+                return cache.get(userId);
+            }
+            try {
+                Thread.sleep(SLEEP_TIME);
+                UserData userData = new UserData(userId, "Details for " + userId);
+                cache.put(userId, userData);
+                return userData;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, executor);
     }
 }
